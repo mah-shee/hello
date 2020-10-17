7b6a60f92fc406d41ce7020c24b79fcb9543eb3d
masa <masatoshi.sboard@gmail.com>
2020-10-17 21:27:35 +0900

    multi thread

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,5 @@
+extern crate hello;
+use hello::ThreadPool;
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -7,10 +9,14 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -21,7 +27,7 @@ fn handle_connection(mut stream: TcpStream) {
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n{}", "hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
